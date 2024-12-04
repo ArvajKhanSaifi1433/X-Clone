362d62583877c72369f19ab8d6c8d53a2099ac46
ArvajKhanSaifi1433 <arvajkhan91@gmail.com>
2024-12-04 22:09:25 +0530

    envirment variable change in frontend

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,28 +1,31 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const TWEET_API_END_POINT = "http://localhost:8000/api/v1/tweet";
+export const USER_API_END_POINT =
+  "https://arvaj-saifi-tweeter-clone.onrender.com/api/v1/user";
+export const TWEET_API_END_POINT =
+  "https://arvaj-saifi-tweeter-clone.onrender.com/api/v1/tweet";
 
+// http://localhost:8000
 
 export const timeSince = (timestamp) => {
-    let time = Date.parse(timestamp);
-    let now = Date.now();
-    let secondsPast = (now - time) / 1000;
-    let suffix = 'ago';
+  let time = Date.parse(timestamp);
+  let now = Date.now();
+  let secondsPast = (now - time) / 1000;
+  let suffix = "ago";
 
-    let intervals = {
-        year: 31536000,
-        month: 2592000,
-        week: 604800,
-        day: 86400,
-        hour: 3600,
-        minute: 60,
-        second: 1
-    };
+  let intervals = {
+    year: 31536000,
+    month: 2592000,
+    week: 604800,
+    day: 86400,
+    hour: 3600,
+    minute: 60,
+    second: 1,
+  };
 
-    for (let i in intervals) {
-        let interval = intervals[i];
-        if (secondsPast >= interval) {
-            let count = Math.floor(secondsPast / interval);
-            return `${count} ${i}${count > 1 ? 's' : ''} ${suffix}`;
-        }
+  for (let i in intervals) {
+    let interval = intervals[i];
+    if (secondsPast >= interval) {
+      let count = Math.floor(secondsPast / interval);
+      return `${count} ${i}${count > 1 ? "s" : ""} ${suffix}`;
     }
-}
+  }
+};
